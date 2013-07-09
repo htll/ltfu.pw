d54b2018ca7edc822ad21083fcffdd654cd6dc82
S0lll0s <SMOArena@gmx.de>
2013-07-10 00:36:59 +0200

    slight styling, JS, renamed stylesheet to screen.css

--- index.php
+++ index.php
@@ -3,21 +3,24 @@
 	<head>
 		<title>URL shortener</title>
 		<meta name="robots" content="noindex, nofollow">
-		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="screen.css" />
+		<script type="text/javascript" src="jquery.min.js"></script>
 		<script type="text/javascript">
-		$(document).ready(function () {
-			$('#shortener').submit(function () {
-				$.ajax({data: {longurl: $('#longurl').val()}, url: 'shorten.php', complete: function (XMLHttpRequest, textStatus) {
-					$('#longurl').val(XMLHttpRequest.responseText);
-				}});
-				return false;
+			$(document).ready(function () {
+				$('#shortener').submit(function () {
+					$.ajax({data: {longurl: $('#longurl').val()}, url: 'shorten.php', complete: function (XMLHttpRequest, textStatus) {
+						$('#shorturl span').val(XMLHttpRequest.responseText);
+					}});
+					$('#shorturl').fadeIn();
+					return false;
+				});
 			});
-		});
 		</script>
 	</head>
 	<body>
 		<form method="post" action="shorten.php" id="shortener">
-			<label for="longurl">URL to shorten</label> <input type="text" name="longurl" id="longurl" /> <input type="submit" value="Shorten" />
+			<label for="longurl" >URL to shorten (http(s)://)</label> <input type="text" name="longurl" id="longurl" /> <input type="submit" value="Shorten" /><br/>
 		</form>
+		<span id="shorturl" >Shortened URL:<span></span></span>
 	</body>
 </html>
