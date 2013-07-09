6a9b3d7dcecd0537764720231c2ef3f754fdda48
S0lll0s <SMOArena@gmx.de>
2013-07-10 00:59:12 +0200

    Style Upgrade

--- index.php
+++ index.php
@@ -18,9 +18,27 @@
 		</script>
 	</head>
 	<body>
-		<form method="post" action="shorten.php" id="shortener">
-			<label for="longurl" >URL to shorten (http(s)://)</label> <input type="text" name="longurl" id="longurl" /> <input type="submit" value="Shorten" /><br/>
-		</form>
-		<span id="shorturl" >Shortened URL:<span></span></span>
+		<a href="https://github.com/lethemfindus/ltfu.pw"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_left_white_ffffff.png" alt="Fork me on GitHub"></a>		
+		<header>
+			<h1 id="title">ltfu.pw</h1>
+			<p>
+				The Original <a href="http://lethemfind.us/community/">lethemfind.us</a> URL Shortener
+			</p>
+		</header>
+		<article>
+			<form method="post" action="shorten.php" id="shortener">
+				<label for="longurl" >URL to shorten (http(s)://)</label> <input type="text" name="longurl" id="longurl" /><br/>
+				<input type="submit" value="Shorten" />
+			</form>
+			<span id="shorturl" >Shortened URL:<span></span></span>
+		</article>
+		<footer>
+			<span id="left">
+				<a href="https://github.com/lethemfindus/ltfu.pw">View on Github</a><br/>
+				Download <a href="https://github.com/lethemfindus/ltfu.pw/zipball/master">zip</a> <a href="https://github.com/lethemfindus/ltfu.pw/master">tar</a>
+			</span>
+			ltfu.pw by <a href="http://lethemfind.us/community/user/824-Miku/">Miku</a>, design by <a href="http://lethemfind.us/community/user/824-1nsignia/">1nsignia aka S0lll0s</a><br/>
+			Visit our friendly community at <a href="http://lethemfind.us/community/">lethemfind.us</a>
+		</footer>
 	</body>
 </html>
